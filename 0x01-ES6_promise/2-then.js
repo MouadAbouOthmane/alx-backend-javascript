ae1c28354126bb4a716778497ff62aa34f738a0b
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-03-05 14:29:10 +0000

    2. Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      const data = { status: 200, body: "success" };
+      console.log("Got a response from the API");
+      return data;
+    })
+    .catch(() => Error());
+}
