1c05ec37c9bbdeee769a7d6c7f4b2b56a585f246
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-03-05 14:10:31 +0000

    1. Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            const response = { status: 200, body: 'Success' };
+            resolve(response);
+        } else {
+            reject(Error('The fake API is not working currently'))
+        }
+
+      });
+}
