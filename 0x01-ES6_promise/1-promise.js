7b2f63a6aad71fc7c5fb1ca2142158e8bbf6ca7c
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-03-07 09:36:37 +0100

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const response = { status: 200, body: "Success" };
+      const response = { status: 200, body: 'Success' };
       resolve(response);
     } else {
-      reject(Error("The fake API is not working currently"));
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
